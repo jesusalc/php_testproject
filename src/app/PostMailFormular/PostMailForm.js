7525b94781951651a7b6f950c9e9d6627c7401c7
jesusalc <jesusalc@gmail.com>
2017-01-30 15:53:05 +0100

    updated the form with sorter fields

--- src/app/PostMailFormular/PostMailForm.js
+++ src/app/PostMailFormular/PostMailForm.js
@@ -2,52 +2,98 @@
  * In this file, we create a React component
  * which incorporates components provided by Material-UI.
  */
-import React, {Component} from 'react';
-import SelectField from 'material-ui/SelectField';
-import MenuItem from 'material-ui/MenuItem';
-import DatePicker from 'material-ui/DatePicker';
-import Divider from 'material-ui/Divider';
-import Paper from 'material-ui/Paper';
-import areIntlLocalesSupported from 'intl-locales-supported';
-import en from 'react-intl/locale-data/en';
-import fr from 'react-intl/locale-data/fr';
-import de from 'react-intl/locale-data/de';
-import TimePicker from 'material-ui/TimePicker';
-import Toggle from 'material-ui/Toggle';
-import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
-import Dialog from 'material-ui/Dialog';
-import {deepOrange500} from 'material-ui/styles/colors';
-import FlatButton from 'material-ui/FlatButton';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import React, {Component} from 'react'
+import FontIcon from 'material-ui/FontIcon'
+import SelectField from 'material-ui/SelectField'
+import MenuItem from 'material-ui/MenuItem'
+import DatePicker from 'material-ui/DatePicker'
+import Divider from 'material-ui/Divider'
+import Paper from 'material-ui/Paper'
+import areIntlLocalesSupported from 'intl-locales-supported'
+import en from 'react-intl/locale-data/en'
+import fr from 'react-intl/locale-data/fr'
+import de from 'react-intl/locale-data/de'
+import TimePicker from 'material-ui/TimePicker'
+import Toggle from 'material-ui/Toggle'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
+import Dialog from 'material-ui/Dialog'
+import {deepOrange500} from 'material-ui/styles/colors'
+import FlatButton from 'material-ui/FlatButton'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
-const styles = {
+const style = {
+  margin: 12,
+}
+
+const PostMailStyle = {
   container: {
     textAlign: 'center',
     paddingTop: 10,
     width: '100%'
   },
-};
+  iconStyles: {
+    marginRight: 24,
+  }
+}
+
+const ZustelldauerInfoStyle = {
+  container: {
+    textAlign: 'center',
+    paddingTop: 10,
+    width: '100%'
+  }
+}
+
+const BriefGewichtStyle = {
+  textfield: {
+    width: '4em'
+  }
+}
+
+const PacketBreiteStyle = {
+  textfield: {
+    width: '4em'
+  }
+}
+
+const PacketGewichtStyle = {
+  textfield: {
+    width: '4em'
+  }
+}
+
+const PacketHoecheStyle = {
+  textfield: {
+    width: '4em'
+  }
+}
+
+const PacketLaengeStyle = {
+  textfield: {
+    width: '4em'
+  }
+}
 
 const muiTheme = getMuiTheme({
   palette: {
     accent1Color: deepOrange500,
   },
-});
+})
 
-let DateTimeFormat;
+let DateTimeFormat
 
 /**
  * Use the native Intl.DateTimeFormat if available, or a polyfill if not.
  */
 if (areIntlLocalesSupported(['fr', 'fa-IR','de','de-DE'])) {
-  DateTimeFormat = global.Intl.DateTimeFormat;
+  DateTimeFormat = global.Intl.DateTimeFormat
 } else {
-  const IntlPolyfill = require('intl');
-  DateTimeFormat = IntlPolyfill.DateTimeFormat;
-  require('intl/locale-data/jsonp/fr');
-  require('intl/locale-data/jsonp/fa-IR');
+  const IntlPolyfill = require('intl')
+  DateTimeFormat = IntlPolyfill.DateTimeFormat
+  require('intl/locale-data/jsonp/fr')
+  require('intl/locale-data/jsonp/fa-IR')
 }
 
 const disableWeekends = (date) => {
@@ -56,11 +102,18 @@ const disableWeekends = (date) => {
 
 class PostMailForm extends Component {
   constructor(props, context) {
-    super(props, context);
+    super(props, context)
 
     this.state = {
       open: false,
-    };
+    }
+
+
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+    this.handleRequestClose = this.handleRequestClose.bind(this)
+    this.handleTouchTap = this.handleTouchTap.bind(this)
   }
 
   handleRequestClose = () => {
@@ -75,12 +128,19 @@ class PostMailForm extends Component {
     });
   }
 
-
-  state = {
-    value: null,
-  };
-
   handleChange = (event, index, value) => this.setState({value});
+
+  onChange(e) {
+    console.log(e.target.name)
+    console.log(e.target.value)
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  onSubmit(e){
+    e.preventDefault()
+    console.log(this.state)
+    // here sent state to the server by the XMLHttpRequest or By Fetch
+  }
 
   render() {
     const standardActions = (
@@ -94,21 +154,29 @@ class PostMailForm extends Component {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <Paper zDepth={2}>
-          <div style={styles.container}>
-            <Dialog
-              open={this.state.open}
-              title="Zustelldauer"
-                actions={standardActions}
-              onRequestClose={this.handleRequestClose}
-            >
-              Regeln hierzu:
-              <ul>
-                <li>Briefe National 1 Tag, Interational 2 Tage
-                      </li>
-                <li>Pakete National 2 Tage, International 7 Tage
-                </li>
-              </ul>
-            </Dialog>
+          <div style={PostMailStyle.container}>
+            <form onSubmit={this.onSubmit} >
+              <input
+                type="text"
+                name="email"
+                value={this.state.email}
+                onChange={this.onChange}
+              />
+
+              <p
+                value={this.state.email}
+                onChange={this.onChange} >
+              </p>
+
+              <RaisedButton
+                label="Login"
+                type="submit"
+                primary
+                style={style}
+              />
+            </form>
+          </div>
+          <div style={PostMailStyle.container}>
             <h1>Webanwendung Web Paket Postsendungen</h1>
             <h2>Produkt wählen &amp; adressieren</h2>
             <h3>Wohin möchten Sie Ihr Paket verschicken?</h3>
@@ -176,14 +244,30 @@ class PostMailForm extends Component {
                 }).format}
               />
               <br />
-              <RaisedButton
-                key="zustelldauer_info"
-                name="zustelldauer_info"
-                id="zustelldauer_info"
-                label="Zustelldauer Info"
-                secondary={true}
-                onTouchTap={this.handleTouchTap}
-              />
+              <div style={ZustelldauerInfoStyle.container}>
+                <Dialog
+                  open={this.state.open}
+                  title="Zustelldauer"
+                  actions={standardActions}
+                  onRequestClose={this.handleRequestClose}
+                  >
+                  Regeln hierzu:
+                  <ul>
+                    <li>Briefe National 1 Tag, Interational 2 Tage
+                          </li>
+                    <li>Pakete National 2 Tage, International 7 Tage
+                    </li>
+                  </ul>
+                </Dialog>
+                <RaisedButton
+                  key="zustelldauer_info"
+                  name="zustelldauer_info"
+                  id="zustelldauer_info"
+                  label="Zustelldauer Info"
+                  secondary={true}
+                  onTouchTap={this.handleTouchTap}
+                />
+              </div>
               <br />
               <TextField
                 key="zustelldauer"
@@ -226,7 +310,7 @@ class PostMailForm extends Component {
                 <MenuItem key="BEL" value="BEL" primaryText="Belgien" />
                 <MenuItem key="BGR" value="BGR" primaryText="Bulgarien" />
                 <MenuItem key="DNK" value="DNK" primaryText="Dänemark" />
-                <MenuItem key="DEU" value="DEU" primaryText="Deutschland"  selected="selected" />
+                <MenuItem key="DEU" value="DEU" primaryText="Deutschland"/>
                 <MenuItem key="EST" value="EST" primaryText="Estland" />
                 <MenuItem key="FIN" value="FIN" primaryText="Finnland" />
                 <MenuItem key="FRA" value="FRA" primaryText="Frankreich" />
@@ -271,60 +355,99 @@ class PostMailForm extends Component {
                 <MenuItem key="Packet" value="Packet" primaryText="Packet" />
               </SelectField>
               <br />
-              <h3>Wie groß ist das Brief?</h3>
-              <TextField
-                key="brief_gewicht"
-                name="brief_gewicht"
-                id="brief_gewicht"
-                hintText="Brief Gewicht"
-                floatingLabelText="Brief Gewicht"
-                multiLine={false}
-                rows={1}
-              />
-              <br />
-              <h3>Wie groß ist das Packet?</h3>
-              <TextField
-                key="packet_laenge"
-                name="packet_laenge"
-                id="packet_laenge"
-                hintText="Packet Länge"
-                floatingLabelText="Länge"
-                multiLine={false}
-                rows={1}
-              /> cm
-              <div>X</div>
-              <TextField
-                key="packet_breite"
-                name="packet_breite"
-                id="packet_breite"
-                inputStyle="width: 3em;"
-                hintText="Packet Breite"
-                floatingLabelText="Breite"
-                multiLine={false}
-                rows={1}
-              /> cm
-              <div>X</div>
-              <TextField
-                key="packet_hoeche"
-                name="packet_hoeche"
-                id="packet_hoeche"
-                inputStyle="width: 3em;"
-                hintText="Packet Höche"
-                floatingLabelText="Höche"
-                multiLine={false}
-                rows={1}
-              /> cm
-              <br />
-              <br />
+
+              <div style={PostMailStyle.container}>
+                <h3>Wie groß ist das Brief?</h3>
+                <TextField
+                  key="brief_gewicht"
+                  name="brief_gewicht"
+                  id="brief_gewicht"
+                  hintText="Brief Gewicht"
+                  floatingLabelText="Brief Gewicht"
+                  multiLine={false}
+                  rows={1}
+                /> g
+                <br />
+              </div>
+              <div style={PostMailStyle.container}>
+                <h3>Wie groß ist das Packet?</h3>
+                <div style={PostMailStyle.container}>
+                  <FontIcon
+                    className="material-icons"
+                    style={PostMailStyle.iconStyles}
+                  > border_bottom
+                  </FontIcon>
+                  <TextField style={PacketLaengeStyle.textfield}
+                    key="packet_laenge"
+                    name="packet_laenge"
+                    id="packet_laenge"
+                    hintText="Länge"
+                    floatingLabelText="Länge"
+                    multiLine={false}
+                    rows={1}
+                  /> cm
+                </div>
+
+                <div style={PostMailStyle.container}>
+                  <FontIcon
+                    className="material-icons"
+                    style={PostMailStyle.iconStyles}
+                  > call_made
+                  </FontIcon>
+                  <TextField
+                    style={PacketBreiteStyle.textfield}
+                    key="packet_breite"
+                    name="packet_breite"
+                    id="packet_breite"
+                    hintText="Breite"
+                    floatingLabelText="Breite"
+                    multiLine={false}
+                    rows={1}
+                  /> cm
+                </div>
+
+                <div style={PostMailStyle.container}>
+                  <FontIcon
+                    className="material-icons"
+                    style={PostMailStyle.iconStyles}
+                  > border_left
+                  </FontIcon>
+                  <TextField
+                    style={PacketHoecheStyle.textfield}
+                    key="packet_hoeche"
+                    name="packet_hoeche"
+                    id="packet_hoeche"
+                    hintText="Höche"
+                    floatingLabelText="Höche"
+                    multiLine={false}
+                    rows={1}
+                  />  cm
+                  <br />
+                </div>
+                <div style={PostMailStyle.container}>
+                  <h3>Wie schwer ist dein Packet?</h3>
+                  <TextField
+                    style={PacketGewichtStyle.textfield}
+                    key="packet_gewicht"
+                    name="packet_gewicht"
+                    id="packet_gewicht"
+                    hintText="Gewicht"
+                    floatingLabelText="Gewicht"
+                    multiLine={false}
+                    rows={1}
+                    /> g
+                    <br />
+                </div>
+              </div>
               <RaisedButton
                 key="versand_submit"
                 name="versand_submit"
                 id="versand_submit"
                 label="Versand des Postpaketregisters"
                 type="submit"
+                primary
                 secondary={false}
               />
-
               <br />
               <br />
             </form>
