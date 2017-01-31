7138953adefc9035088470c485b63de1e78fe583
jesusalc <jesusalc@gmail.com>
2017-01-31 12:36:26 +0100

    customized date selector to auto close

--- src/app/PostMailFormular/fields/EmpfaengerDatum.js
+++ src/app/PostMailFormular/fields/EmpfaengerDatum.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react'
 import DatePicker from 'material-ui/DatePicker'
 import areIntlLocalesSupported from 'intl-locales-supported'
+import {addLocaleData} from 'react-intl';
 import en from 'react-intl/locale-data/en'
 import fr from 'react-intl/locale-data/fr'
 import de from 'react-intl/locale-data/de'
 import TimePicker from 'material-ui/TimePicker'
+
+addLocaleData(en);
+addLocaleData(fr);
+addLocaleData(de);
 
 let DateTimeFormat
 
@@ -18,6 +23,8 @@ if (areIntlLocalesSupported(['fr', 'fa-IR','de','de-DE'])) {
   DateTimeFormat = IntlPolyfill.DateTimeFormat
   require('intl/locale-data/jsonp/fr')
   require('intl/locale-data/jsonp/fa-IR')
+  require('intl/locale-data/jsonp/de')
+  require('intl/locale-data/jsonp/de-DE')
 }
 
 const disableWeekends = (date) => {
@@ -28,8 +35,22 @@ class EmpfaengerDatum extends Component {
   constructor(props, context) {
     super(props, context)
 
+    const minDate = new Date()
+    const maxDate = new Date()
+
+    minDate.setFullYear(minDate.getFullYear() - 1)
+    minDate.setHours(0, 0, 0, 0)
+    maxDate.setFullYear(maxDate.getFullYear() + 1)
+    maxDate.setHours(0, 0, 0, 0)
+
+    const daysName=['Sontag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag']
+    const monthsName=['Januar', 'Februar',  'März', 'April',  'Mai', 'Juni',  'Juli', 'August',  'September', 'Oktober',  'November', 'Dezember']
+
     this.state = {
-      value: "",
+      minDate: minDate,
+      maxDate: maxDate,
+      autoOk: true,
+      disableYearSelection: false,
     }
   }
 
@@ -44,11 +65,15 @@ class EmpfaengerDatum extends Component {
                   errorText="Dieses Feld wird benötigt."
                   container="inline"
                   mode="landscape"
+                  minDate={this.state.minDate}
+                  maxDate={this.state.maxDate}
+                  disableYearSelection={this.state.disableYearSelection}
                   shouldDisableDate={disableWeekends}
-                  locale="de-DE"
                   okLabel="Nutzen"
+                  defaultDate={this.state.minDate}
                   cancelLabel="Beenden"
                   firstDayOfWeek={1}
+                  autoOk={this.state.autoOk}
                   formatDate={new DateTimeFormat('de', {
                     day: 'numeric',
                     month: 'numeric',
